961abf15f635c0c325a6ada8fdb7767135c820ea
mohammad shahid <129838380+snorlaax@users.noreply.github.com>
2023-04-04 15:43:24 +0530

    Add files via upload

---
+++ assignment1question1.py
@@ -0,0 +1,5 @@
+s1=float(input("Enter one number:"))
+s2=float(input("Enter another number:"))
+s3=float(input("Enter another number:"))
+avg=(s1+s2+s3)/3
+print("Average of three numbers are:",avg)
